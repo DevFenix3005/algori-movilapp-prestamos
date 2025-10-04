d3df90d4e7e3527c4517ee6230b9630ee41d5605
Roberto D. Cazarin <robertodmngzczrn@gmail.com>
2025-10-04 12:01:43 -0600

    Avance de la aplicacion
    Distribuye los elementos de UI y se agregan validaciones de entrada por el usuario

--- main_movil.py
+++ main_movil.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
@@ -11,35 +12,58 @@ class HomeScreen(Screen):
     
     def __init__(self, **kw):
         super().__init__(**kw)
-        contenedor = BoxLayout(orientation="vertical")
+        contenedor = BoxLayout(orientation="vertical", padding=8, spacing=8)
                                                     # X, Y
-        self.monto_prestamo_input = TextInput(size_hint=(1, 0.25))
-        self.tasa_interes_input = TextInput(size_hint=(1, 0.25))
-        self.plazo_input = TextInput(size_hint=(1, 0.25))
-        calculo_button = Button(text="Calcular")
+        self.monto_prestamo_input = TextInput(size_hint=(1, 0.25), multiline=False, hint_text="Monto del prestamo", font_size=20)
+        self.tasa_interes_input = TextInput(size_hint=(1, 0.25), multiline=False, hint_text="Interes del prestamo (%)", font_size=20)
+        self.plazo_input = TextInput(size_hint=(1, 0.25), multiline=False, hint_text="Plazo en año", font_size=20)
+
+        calculo_button = Button(text="Calcular", size_hint=(1, 0.25))
         calculo_button.on_press = self.calculo_prestamo_handler
+
+        amortizacion_button = Button(text="Tablas Amortizacion", size_hint=(1, 0.25))
+        sub_contenedor_botones = BoxLayout(padding=8, spacing=8)
+        sub_contenedor_botones.add_widget(calculo_button)
+        sub_contenedor_botones.add_widget(amortizacion_button)
+
+
+        contenedor_botones = AnchorLayout(anchor_y='bottom')
+        contenedor_botones.add_widget(sub_contenedor_botones)
         
         contenedor.add_widget(self.monto_prestamo_input)
         contenedor.add_widget(self.tasa_interes_input)
         contenedor.add_widget(self.plazo_input)
-        contenedor.add_widget(calculo_button)
+        contenedor.add_widget(contenedor_botones)
         self.add_widget(contenedor)
 
+    def obtener_datos(self):
+        monto = self.monto_prestamo_input.text
+        interes = self.tasa_interes_input.text
+        plazo = self.plazo_input.text
+        return monto, interes, plazo
+
     def calculo_prestamo_handler(self):
-        print("Se preciono el boton")
-        monto_prestamo = float(self.monto_prestamo_input.text)
-        tasa_interes = float(self.tasa_interes_input.text)
-        plazo = int(self.plazo_input.text)
-        print(f"Monto prestamo: {monto_prestamo}")
-        print(f"Tasa interes: {tasa_interes}")
-        print(f"Plazo: {plazo}")
-        
-        cuota, total = calcular_cuota(monto_prestamo, tasa_interes, plazo)
-        texto_calculo = f"Pago mensual: ${cuota:,.2f}\nTotal a pagar: ${total:,.2f}"
-        popup = Popup(title='Calculo del prestamo',
-            content=Label(text=texto_calculo),
-            size_hint=(None, None), size=(400, 400))
-        popup.open()
+        m, i, p = self.obtener_datos()
+        if m and i and p:
+            monto_prestamo = float(m)
+            tasa_interes = float(i)
+            plazo = int(p)
+            print(f"Monto prestamo: {monto_prestamo}")
+            print(f"Tasa interes: {tasa_interes}")
+            print(f"Plazo: {plazo}")
+            
+            cuota, total = calcular_cuota(monto_prestamo, tasa_interes, plazo)
+            texto_calculo = f"Pago mensual: ${cuota:,.2f}\nTotal a pagar: ${total:,.2f}"
+            popup = Popup(title='Calculo del prestamo',
+                content=Label(text=texto_calculo),
+                size_hint=(None, None), size=(400, 400))
+            popup.open()
+        else:
+            popup = Popup(title='La validacion ha fallado',
+                content=Label(text="Ingresa valores en los campos monto, tasa, plazo"),
+                size_hint=(None, None), size=(400, 400))
+            popup.open()
+            
         
 class Graph1Screen(Screen):
     
